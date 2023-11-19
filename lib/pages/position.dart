436b9725a7d87f12a3b60e671b1bad1195af8a61
Rene Vallecillo <reneval@gmail.com>
2023-11-18 20:41:34 -0600

    working geolocation

---
+++ lib/pages/position.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:trace/services/geolocation.dart';
+
+class PositionPage extends StatefulWidget {
+  const PositionPage({Key? key}) : super(key: key);
+
+  @override
+  _PositionState createState() => _PositionState();
+}
+
+class _PositionState extends State<PositionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StreamBuilder(
+              stream: GeoLocationService.positionStream,
+              // initialData: ,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Text('Loading...');
+                }
+                return Text(
+                    'Location: ${snapshot.data?.latitude}, ${snapshot.data?.longitude}');
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
